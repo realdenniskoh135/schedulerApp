d38909f0cd07a9220b0c0528f052ee2afcbfbab4
Dennis Koh <dkoh135@berkeley.edu>
2024-08-25 17:47:06 -0400

    page

--- scheduler/app/src/landing-page/LandingPage.tsx
+++ scheduler/app/src/landing-page/LandingPage.tsx
@@ -4,10 +4,7 @@ import Schedule from './pages/MySchedule';
 import OneToOneCall from './pages/OneToOneCall';
 import PriorityEmail from './pages/PriorityEmail';
 import JoinCall from './pages/JoinCall';
-<<<<<<< HEAD
 import { useAuth } from 'wasp/client/auth';
-=======
->>>>>>> a611305392a06a4890c81a73841c0c95ed3f2233
 
 const landingPageNavigation = [
   { name: 'My Schedule', href: '/my-schedule' },
@@ -17,20 +14,13 @@ const landingPageNavigation = [
 ];
 
 export default function LandingPage() {
-<<<<<<< HEAD
   const { data: user } = useAuth();
-=======
->>>>>>> a611305392a06a4890c81a73841c0c95ed3f2233
   return (
     <div className='bg-white dark:text-white dark:bg-boxdark-2'>
       <Header navigation={landingPageNavigation} />
 
       <main className='isolate dark:bg-boxdark-2'>
-<<<<<<< HEAD
       <Hero />
-=======
-        <Hero />
->>>>>>> a611305392a06a4890c81a73841c0c95ed3f2233
       </main>
     </div>
   );
